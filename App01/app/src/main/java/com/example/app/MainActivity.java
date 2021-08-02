c02f61bdee37e514dcd2eb8d631b656dc6c5e340
sk421120 <sk421120@naver.com>
2021-08-02 17:51:06 +0900

    imageView

---
+++ App01/app/src/main/java/com/example/app/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // xml에 설정된 ImageView를 Java Code에서 사용하기 위한 선언
+    private ImageView myImage = null;
+
+    // 이미지를 교체하면서 보여주기 위한 변수선언
+    private int count = 0;
+    // 이미지 정보를 담을 배열
+    private int[] images;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // 선언된 ImageView에 xml에 설정된 View를 연결하기
+        myImage = findViewById(R.id.my_image);
+
+        // drawable 폴더에 있는 이미지 정보를 배열에 담기
+        images = new int[3];
+        images[0] = R.drawable.test;
+        images[1] = R.drawable.test1;
+        images[2] = R.drawable.test2;
+
+        // ImageView를 클릭했을때
+        myImage.setOnClickListener(view -> {
+            // count 변수를 1씩 증가하고
+            count++;
+            // 증가된 count 변수를 숫자 3으로 나머지 연산 수행
+            // 0, 1, 2 중의 1개의 값이 된다
+            int imgNum = count % 3;
+
+            // 3개의 배열에 담긴 정보중에 imgNum요소의 정보를 ImageView에 세팅하기기
+           myImage.setImageResource(images[imgNum]);
+        });
+    }
+}
